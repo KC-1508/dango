f75036b20e7975dfc65788b44ebb6b9c039d49dc
KC-1508 <107610683+KC-1508@users.noreply.github.com>
2022-07-05 12:18:45 -0500

    feat: fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+version '1.0.0-BETA' --[[
+    This is a BETA build, and is recommended for development ONLY.
+]]
+game 'gta5'
+
+author 'KC-1508'
+repository 'https://github.com/KC-1508/dango'
+description '🍡 Open source miscellaneous collection for 5M.'
+
+client_scripts {
+    'client/cl_*.lua'
+}
+
+server_scripts {
+    'server/sv_*.lua'
+}
